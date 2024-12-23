473e9d0abd1bdd01b4d13732799448eba78b97f8
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2024-12-23 14:42:17 +0800

    Update script.js

--- utils/script.js
+++ utils/script.js
@@ -1,19 +1,7 @@
 const messages = [
     "Hi love:)",
-    "Kamusta naman akong palangga? Is she getting better every single day ba?",
-    "Well i hope so... i need my lalove to be strong and independent habang wala pa iyahang night in shining agtang hahaha.",
-    "Pero enough of that silly sht...",
-    "I really wanted show you how i love you and i wanna make you safe with me, diko gusto maging bad example satong future self nanong nagbuwag ta or anything...",
-    "I just want you to know i still have this same feelings for you, since day one^^",
-    "And this is the reason why i made you this website:)",
-    "Since wamay maka appreciate aning lowkey nako na talent, I'll just spoil it to you nalang since you wanted it so bad:)",
-    "Mupadayon gihapon kog himog ingani nga content in the near future, as long as its for you, i will keep myself motivated:)",
-    "And if you ever feel like coming back to this website, pwede kaayo love.",
-    "After all, i made this for you:>",
-    "I love you langga, unta you really are the one na, unta ikaw najud magpa tunay sako nga you are worth waiting for.",
-    "That's all muna love... May our relationship be stronger than bricks and stones...",
-    "I love you...❤️",
-    "Your's truly, your night in shining agtang."
+    "You know that i love you so much",
+    "This last message will direct the user to a youtube link( Feel free to edit it in /utils/script.js )"
 ];
 
 let currentMessageIndex = 0;
